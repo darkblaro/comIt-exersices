fc70a510e9c0769007a6fe035403d2a294ac6d44
darkblaro <darkblaro@gmail.com>
2020-09-19 00:45:08 -0500

    Exercise 20 added

---
+++ JavaScript/Exercise 20/index.js
@@ -0,0 +1,18 @@
+let age=36;
+let firstName="Roman";
+let courseCompleted=false;
+let finalScore=null;
+let winningNumbers;
+
+console.log(age);
+console.log(firstName);
+console.log(courseCompleted);
+console.log(finalScore);
+console.log(winningNumbers);
+
+console.log(String(age));
+console.log(Boolean(firstName));
+console.log(Boolean(age));
+console.log(Number(winningNumbers));
+console.log(Number(courseCompleted));
+console.log(String(finalScore));
