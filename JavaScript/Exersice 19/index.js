15dc92050143ec2fa2a8414fb110e760514c9a4e
darkblaro <darkblaro@gmail.com>
2020-09-19 00:44:47 -0500

    Exercise 19 added

---
+++ JavaScript/Exersice 19/index.js
@@ -0,0 +1,19 @@
+const monthOfBirth="July";
+const firstName="Roman";
+const lastName="Blagovestny";
+
+let city;
+let age;
+let nextCity;
+
+city="Winnipeg";
+age=36;
+nextCity=city;
+
+console.log('First name: ',firstName);
+console.log('Last name: ',lastName);
+console.log('Month of Birth: ',monthOfBirth);
+console.log('City: ',city);
+console.log('Age: ',age);
+console.log('Next City: ',nextCity);
+console.log(firstName,' ',lastName);
